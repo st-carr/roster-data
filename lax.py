2fd62b209897644487046efb513472ec7f846df9
Steve <scarr087@gmail.com>
2017-06-24 11:03:25 -0700

    change 5

--- lax.py
+++ lax.py
@@ -16,17 +16,21 @@ def main():
     recheck_header_child = []
     list_of_index_dict = []
     index_dict = {}
+    num = 0
+
 
     #Text file containing University name, league, and roster URL
-    f_read = open("sites.txt", "rt")
-    writing = open("writing_output", "w")
-    player_out = open("player_output", "w")
+    f_read = open("ivy.txt", "rt")
+    writing = open("ivy_writing_output", "a")
+    player_out = open("ivy_player_output", "w")
     data = []
     #Dictionary variable containing individual player data, which is then transcribed to JSON
-    player_data = dict()
-
+    #player_data = {"position": "", "year": "", "number": "", "height": "", "weight": "", "state": ""}
+    player_model = {}
+    data = []
+    player_data = {}
     #Create single JSON output file
-    total_output = open("final_player_data.json", "w")
+    total_output = open("ivy_player_data.json", "w")
 
     #CSV object of University text file
     reader = csv.reader(f_read)
@@ -136,25 +140,154 @@ def main():
         #Iterate over each HTML table row
         
         for tr in trs:
-            player_out.write(player_data["school"] + "\n")
             #Find all HTML table data cells
             tds = tr.find_all("td")
             #Iterate over each table data cell
             for index_0, td in enumerate(tds):
+                #Reads in variations on State names and abbreviations from a CSV file
+                f_state = open("states.txt", "rt")
+                states = csv.reader(f_state)
+                for state in states:
+                    for name in state:
+                        #Looks for State name or abbreviation in stripped table data cell
+                        state_check = None
+                        state_check = re.search(r', ' + re.escape(name), td.get_text().replace("\n", "").replace("\t", "").strip())
+                        if state_check:
+                            player_data['state'] = state[0]
+
                 td_data = td.get_text().replace("\n", "").replace("\t", "").strip()
                 if index_0 == player_number_index:
-                    player_out.write("Number: " + str(td_data) + "\n")
+                    player_data["number"] = str(td_data)
                 elif index_0 == player_name_index:
-                    player_out.write("Name: " + str(td_data) + "\n")
+                    player_data["name"] = str(td_data)
                 elif index_0 == player_pos_index:
-                    player_out.write("Position: " + str(td_data) + "\n")
+                    a_pos = re.search(r'a$|A$|Attack$|attack$', str(td_data))
+                    d_pos = re.search(r'd$|D$|Defense$|defense$', str(td_data))
+                    g_pos = re.search(r'g$|G$|GK$|Gk$|gk$|Goalie$', str(td_data))
+                    m_pos = re.search(r'm$|M$|Middie$|middie$', str(td_data))
+                    lsm_pos = re.search(r'LSM$|lsm$', str(td_data))
+                    am_pos = re.search(r'A\/M$|a\/m$\M\/A$|m\/a$', str(td_data))
+                    fo_pos = re.search(r'F\/O|Face-off$|Face-Off$|FO$|FO\/M$|M\/FO$', str(td_data))
+                    if a_pos:
+                        player_data["position"] = "Attack"
+                    elif d_pos:
+                        player_data["position"] = "Defense"
+                    elif g_pos:
+                        player_data["position"] = "Goalie"
+                    elif m_pos:
+                        player_data["position"] = "Midfield"
+                    elif lsm_pos:
+                        player_data["position"] = "Long Stick Midfield"
+                    elif am_pos:
+                        player_data["position"] = "Attack/Midfield"
+                    elif fo_pos:
+                        player_data["position"] = "FaceOff"
+                    else:
+                        player_data["position"] = ""
                 elif index_0 == player_ht_index:
-                    player_out.write("Height: " + str(td_data) + "\n")
+                    player_data['height'] = str(td_data)
                 elif index_0 == player_wt_index:
-                    player_out.write("Weight: " + str(td_data) + "\n")
+                    player_data["weight"] = str(td_data)
                 elif index_0 == player_year_index:
-                    player_out.write("Year: " + str(td_data) + "\n")
-            player_out.write("\n")
+                    fr_year = re.search(r'fr\.$|Fr\.$|fr$|Fr$|^freshman$|^Freshman$', str(td_data))
+                    so_year = re.search(r'so\.$|So\.$|so$|So$|^sophomore$|^Sophomore$', str(td_data))
+                    jr_year = re.search(r'jr\.$|Jr\.$|jr$|Jr$|^junior$|^Junior$', str(td_data))
+                    sr_year = re.search(r'sr\.$|Sr\.$|Sr$|Sr$|^Senior$|^Senior$', str(td_data))
+                    if fr_year:
+                        player_data["year"] = "Freshman"
+                    elif so_year:
+                        player_data["year"] = "Sophomore"
+                    elif jr_year:
+                        player_data["year"] = "Junior"
+                    elif sr_year:
+                        player_data["year"] = "Senior"
+
+        
+        if player_data["number"] and player_data["position"] and player_data["height"] and player_data["weight"] and player_data["year"]:
+            num += 1
+            player_model = {"model": "player_data.Player",
+                            "pk": num,
+                            "fields": player_data.copy()}
+            data.append(player_model.copy())
+
+        mismatch = re.search(r'^\d{1,2}$', player_data["name"])
+        if mismatch:
+            print(str(player_data["school"]) + ": MISMATCH")
+            for tr in trs:
+                #Find all HTML table data cells
+                tds = tr.find_all("td")
+                #Iterate over each table data cell
+                for index_0, td in enumerate(tds):
+                    if index_0 == player_number_index:
+                        player_data["number"] = str(tds[index_0 + 1].get_text().replace("\n", "").replace("\t", "").strip())
+                    elif index_0 == player_name_index:
+                        player_data["name"] = str(tds[index_0 + 1].get_text().replace("\n", "").replace("\t", "").strip())
+                    elif index_0 == player_pos_index:
+                        a_pos = re.search(r'a$|A$|Attack$|attack$', str(tds[index_0 + 1].get_text().replace("\n", "").replace("\t", "").strip()))
+                        d_pos = re.search(r'd$|D$|Defense$|defense$', str(tds[index_0 + 1].get_text().replace("\n", "").replace("\t", "").strip()))
+                        g_pos = re.search(r'g$|G$|GK$|Gk$|gk$|Goalie$', str(tds[index_0 + 1].get_text().replace("\n", "").replace("\t", "").strip()))
+                        m_pos = re.search(r'm$|M$|Middie$|middie$', str(tds[index_0 + 1].get_text().replace("\n", "").replace("\t", "").strip()))
+                        lsm_pos = re.search(r'LSM$|lsm$', str(tds[index_0 + 1].get_text().replace("\n", "").replace("\t", "").strip()))
+                        am_pos = re.search(r'A\/M$|a\/m$\M\/A$|m\/a$', str(tds[index_0 + 1].get_text().replace("\n", "").replace("\t", "").strip()))
+                        fo_pos = re.search(r'F\/O|Face-off$|Face-Off$|FO$|FO\/M$|M\/FO$', str(tds[index_0 + 1].get_text().replace("\n", "").replace("\t", "").strip()))
+                        if a_pos:
+                            player_data["position"] = "Attack"
+                        elif d_pos:
+                            player_data["position"] = "Defense"
+                        elif g_pos:
+                            player_data["position"] = "Goalie"
+                        elif lsm_pos:
+                            player_data["position"] = "Long Stick Midfield"
+                        elif m_pos:
+                            player_data["position"] = "Midfield"
+                        elif am_pos:
+                            player_data["position"] = "Attack/Midfield"
+                        elif fo_pos:
+                            player_data["position"] = "FaceOff"
+                        else:
+                            player_data["position"] = ""
+                    elif index_0 == player_ht_index:
+                            player_data["height"] = str(tds[index_0 + 1].get_text().replace("\n", "").replace("\t", "").strip())
+                    elif index_0 == player_wt_index:
+                        player_data["weight"] = str(tds[index_0 + 1].get_text().replace("\n", "").replace("\t", "").strip())
+                    elif index_0 == player_year_index:
+                        fr_year = re.search(r'fr\.$|Fr\.$|fr$|Fr$|^freshman$|^Freshman$', str(tds[index_0 + 1].get_text().replace("\n", "").replace("\t", "").strip()))
+                        so_year = re.search(r'so\.$|So\.$|so$|So$|^sophomore$|^Sophomore$', str(tds[index_0 + 1].get_text().replace("\n", "").replace("\t", "").strip()))
+                        jr_year = re.search(r'jr\.$|Jr\.$|jr$|Jr$|^junior$|^Junior$', str(tds[index_0 + 1].get_text().replace("\n", "").replace("\t", "").strip()))
+                        sr_year = re.search(r'sr\.$|Sr\.$|Sr$|Sr$|^Senior$|^Senior$', str(tds[index_0 + 1].get_text().replace("\n", "").replace("\t", "").strip()))
+                        if fr_year:
+                            player_data["year"] = "Freshman"
+                        elif so_year:
+                            player_data["year"] = "Sophomore"
+                        elif jr_year:
+                            player_data["year"] = "Junior"
+                        elif sr_year:
+                            player_data["year"] = "Senior"
+                        else:
+                            player_data["year"] = " "
+                            
+                num += 1
+                player_model = {"model": "player_data.Player",
+                                "pk": num,
+                                "fields": player_data.copy()}
+                data.append(player_model.copy())
+
+    json.dump(data, total_output, sort_keys=True, indent=4)
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
+
+"""
+height_match = re.search(r'^(\d)-(\d{1,2})$', str(tds[index_0 + 1].get_text().replace("\n", "").replace("\t", "").strip()))
+feet = int(height_match.group(1))
+inches = int(height_match.group(2))
+total_height = (feet * 12) + inches
+player_data['height'] = int(total_height)
+"""
